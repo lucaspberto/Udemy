ab6258ea38711550faee3d6c4bd09f8e7107707a
Lucas Berto <lucaspb.u2@gmail.com>
2022-03-17 14:34:46 -0300

    Initial commit

---
+++ Udemy/Udemy/Shared/UdemyApp.swift
@@ -0,0 +1,18 @@
+//
+//  UdemyApp.swift
+//  Shared
+//
+//  Created by Lucas Berto on 17/03/22.
+//
+//
+
+import SwiftUI
+
+@main
+struct UdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
